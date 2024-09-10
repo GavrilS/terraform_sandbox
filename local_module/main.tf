5d8299774d1dcdbc8cdac354eeb3a35a7255878e
GavrilS <gavr1lll@abv.bg>
2024-09-10 23:52:05 +0300

    Outputing values of files

--- local_module/main.tf
+++ local_module/main.tf
@@ -1,2 +1,10 @@
 provider "local" {}
 
+resource "local_file" "test" {
+  content  = "This is a test!"
+  filename = "${path.module}/local_resources/test.txt"
+}
+
+data "local_file" "test" {
+  filename = "${path.module}/local_resources/test.txt"
+}
--- local_module/outputs.tf
+++ local_module/outputs.tf
@@ -1,3 +1,7 @@
 output "test_ssh_dir" {
   value = provider::local::direxists(pathexpand("~/.ssh"))
 }
+
+output "local_resource_value" {
+  value = data.local_file.test.content
+}
