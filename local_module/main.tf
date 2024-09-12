69cc1dd40fbce17ecd5c47c451b0f6a801b685bc
GavrilS <gavr1lll@abv.bg>
2024-09-12 22:31:37 +0300

    Template testing

--- local_module/main.tf
+++ local_module/main.tf
@@ -1,10 +1,19 @@
 provider "local" {}
 
+provider "template" {}
+
 resource "local_file" "test" {
-  content  = "This is a test!"
+  content  = template_file.greeting.rendered
   filename = "${path.module}/local_resources/test.txt"
 }
 
 data "local_file" "test" {
   filename = "${path.module}/local_resources/test.txt"
+}
+
+resource "template_file" "greeting" {
+  template = file("templates/greeting.txt")
+  vars = {
+    name = "John Doe"
+  }
 }
